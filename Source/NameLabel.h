d43f6d26f172d5aab34be41dfa127eb22505d1ce
Andrew King <andrewking1597@gmail.com>
2021-11-18 16:19:31 -0500

    clean up

--- Source/NameLabel.h
+++ Source/NameLabel.h
@@ -29,17 +29,12 @@ public:
     ~NameLabel();
     
 private:
-    juce::Colour blue = juce::Colour::fromFloatRGBA(0.43f, 0.83f, 1.0f, 1.0f);
     juce::Colour grey = juce::Colour::fromFloatRGBA(0.42f, 0.42f, 0.42f, 1.0f);
     juce::Colour blackGrey = juce::Colour::fromFloatRGBA(0.2f, 0.2f, 0.2f, 1.0f);
     juce::Colour offWhite = juce::Colour::fromFloatRGBA(0.83f, 0.84f, 0.9f, 1.0f);
-    juce::Colour mint = juce::Colour::fromFloatRGBA(0.54f, 1.0f, 0.76f, 1.0f);
-    juce::Colour fireRed = juce::Colour::fromFloatRGBA(0.93f, 0.38f, 0.33f, 1.0f);
-    juce::Colour abkBlue = juce::Colour::fromRGB(5, 17, 242);
-    juce::Colour abkRed = juce::Colour::fromRGB(242, 5, 5);
-    juce::Colour abkGreen = juce::Colour::fromRGB(90, 157, 59);
-    juce::Colour abkYellow = juce::Colour::fromRGB(233, 215, 88);
-    juce::Colour abkPeach = juce::Colour::fromRGB(247, 178, 183);
-    juce::Colour abkPink = juce::Colour::fromRGB(212, 17, 208);
-    juce::Colour abkPurple = juce::Colour::fromRGB(61, 20, 245);
+    juce::Colour offWhite2 = juce::Colour::fromRGB(229, 229, 229);
+    juce::Colour newGreen = juce::Colour::fromRGB(62, 218, 121);
+    juce::Colour newRed = juce::Colour::fromRGB(249, 62, 59);
+    juce::Colour newBlue = juce::Colour::fromRGB(13, 81, 230);
+    juce::Colour newPink = juce::Colour::fromRGB(239, 59, 243);
 };
